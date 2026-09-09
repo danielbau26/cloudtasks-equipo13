d16f79ec61e82edf5cd90b415110f2a98f774a41
Jhoan <jhoandm57@gmail.com>
2026-09-09 15:03:38 -0500

    Se empieza la integración de supaBase con js: Se crea el supabaseClient.js con el URL y el anon Key

---
+++ cloudtasks-equipo13/js/supabaseClient.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://rnaaxyenpllnzmpokrcp.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_5hAfdGVhkNDZmm1vtN7UWQ_KT4Pt2Z5";
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
